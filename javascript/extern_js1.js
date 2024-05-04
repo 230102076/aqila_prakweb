d2825cd9bfed43994ae3e03dc30d22e585be8d3f
230102076 <aqilarft@gmail.com>
2024-05-04 12:10:14 +0700

    Add files via upload

---
+++ javascript/extern_js1.js
@@ -0,0 +1,3 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "JavaScript external adalah cara menulis program JavaScript dalam file yang berekstensi.js dan kemudian menautkan file tersebut di dalam tag head atau body pada file HTML";
+  } 
